11dd2d45332b85391e13c8b9f9980ac6c92cd3f6
Paul Welby <pwelby@gmail.com>
2022-01-30 07:47:49 -0800

    updates to main

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -4,22 +4,19 @@ import { Link } from 'react-router-dom'
 
 const ProjectList = ({projects}) => {
 
-
-
   return (
-    <div style={{cursor: 'pointer'}}  className="project-list section">
-      { projects && projects.map(project => {
-        return (
-          <Link
-            to={'/project/' + project.id}
-            key={project.id}
-            >
-            <ProjectSummary project={project} />
-
-          </Link>
-
-        )
-      })}
+    <div className='list-outer-container'>
+      <div style={{cursor: 'pointer'}}  className="project-list section">
+        { projects && projects.map(project => {
+          return (
+            <Link to={'/project/' + project.id} key={project.id}>
+              <ProjectSummary project={project} />
+            </Link>
+          )
+        })}
+      </div>
+      <br/>
+      <small>Click a tile to see details</small>
     </div>
   )
 }
