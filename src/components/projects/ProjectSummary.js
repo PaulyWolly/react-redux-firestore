45301df930eacfeb762b742fd45f9ff18d7f3a79
Paul Welby <pwelby@gmail.com>
2022-01-29 18:09:08 -0800

    initial commit

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import moment from 'moment'
+
+const ProjectSummary = ({project}) => {
+  const timestamp = project && project.createdAt && project.createdAt.toDate();
+  return (
+    <div className="card  project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title ">{project.title}</span>
+        <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
+        <p className="grey-text">{moment(timestamp).calendar()}</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary
